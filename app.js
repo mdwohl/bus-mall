192c412261fda57d83bcab9db67fdfd532a61d9c
mdwohl <mdwohl@gmail.com>
2020-07-20 13:54:05 -0700

    setup

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//constructor function to make an object: name, image src, clicks.
+//constructor needs: a property that holds number of times the product has been clicked; after every selection update the newly added property to indicate it was clicked.
+
+
+//event listener appended to HTML where images are displayed (li, likely)
+
+//on click, generate three new products at random
+
+//ONLY 25 rounds of clicking. Keep this number in a var that can be changed for debugging
+
+//after voting is done, remove event listeners
+
+//display all products with number of vots and number of times shown
+
+//generate three random product images from img dir and display inline
