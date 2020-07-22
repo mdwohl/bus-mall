1b96301e2f5871a7ed1d6094c1fed43656d9cd60
mdwohl <mdwohl@gmail.com>
2020-07-22 13:34:12 -0700

    times shown chart

--- app.js
+++ app.js
@@ -136,6 +136,11 @@ function renderGraphOnPage (){
     productData.push(productArray[j].votes);
   }
 
+  var productTimesShown = [];
+  for(var x = 0; x < productArray.length; x++) {
+    productTimesShown.push(productArray[x].shown);
+  }
+
   var ctx = document.getElementById('myChart').getContext('2d');
   var myChart = new Chart(ctx, {
     type: 'bar',
@@ -144,6 +149,26 @@ function renderGraphOnPage (){
       datasets: [{
         label: '# of Votes',
         data: productData,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }, {
+        label: 'times shown',
+        data: productTimesShown,
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
